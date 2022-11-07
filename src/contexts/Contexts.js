b682b7861d005744e2b3652dd87eb56f8acfdb9c
niloy143 <niloymahmudapu@gmail.com>
2022-11-08 01:57:04 +0600

    hosted on firebase

--- src/contexts/Contexts.js
+++ src/contexts/Contexts.js
@@ -1,10 +1,16 @@
 import React, { createContext } from 'react';
+import app from '../firebase/firebase-init';
+import { getAuth } from 'firebase/auth';
 
 export const TailorContext = createContext({});
+const auth = getAuth(app);
 
 const Contexts = ({ children }) => {
+
+    console.log(auth)
+
     const value = {
-        name: 'Niloy'
+
     }
     return (
         <TailorContext.Provider value={value}>
