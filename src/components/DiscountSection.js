7fcb7ce2c18235fe84250ffe95882aed416789ea
niloy143 <niloymahmudapu@gmail.com>
2022-11-10 23:33:41 +0600

    dummy text to relevant texts

--- src/components/DiscountSection.js
+++ src/components/DiscountSection.js
@@ -6,7 +6,7 @@ const DiscountSection = () => {
             <img className='w-full' src="https://i.ibb.co/jrzNnDJ/sale-off-discount-banner-design-template-promo-tag-extra-offer-vector-illustration-181576103.jpg" alt="" />
             <h2 className='my-5 text-xl sm:text-3xl font-semibold text-center'>Customers get discount for their honest review</h2>
             <p className='text-sm sm:text-base max-w-4xl mx-auto text-justify'>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
+                We give special discounts whoever gives an honest to any of our products that is available in the current time. To get discount in any service, write a review including at least 1000 characters in which service you recently took from us. You are highly recommended to be honest, no need to convince us to get the discount. You can also criticize our services constructively and honestly, we will pay you as the criticisms are always beneficial for us.
             </p>
         </div>
     );
--- src/components/SliderSection.js
+++ src/components/SliderSection.js
@@ -6,9 +6,9 @@ const SliderSection = () => {
     return (
         <div className="max-w-6xl mx-auto">
             <Carousel>
-                <CarouselItem img="https://i.ibb.co/9c6CV5P/Christies-Tailors-Services.jpg" title="Lorem ipsum dolor sit amet." description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam illum, unde eos adipisci optio vitae amet suscipit odit rem ipsa reiciendis neque repellat hic pariatur cum! Sint quaerat debitis commodi voluptate officia architecto dolor quos veritatis! Dolore fugiat illo velit vel est facere qui." />
-                <CarouselItem img="https://i.ibb.co/84t1xJK/pexels-photo-404159.jpg" title="Lorem ipsum dolor sit amet." description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam illum, unde eos adipisci optio vitae amet suscipit odit rem ipsa reiciendis neque repellat hic pariatur cum! Sint quaerat debitis commodi voluptate officia architecto dolor quos veritatis! Dolore fugiat illo velit vel est facere qui." />
-                <CarouselItem img="https://i.ibb.co/Zc1Bx5R/D66-UE7iz-Hh-Sk-Uu-GM08ura3-Dhk-Y9-Mo-U8-UKQDRf-Ma-E1.jpg" title="Lorem ipsum dolor sit amet." description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam illum, unde eos adipisci optio vitae amet suscipit odit rem ipsa reiciendis neque repellat hic pariatur cum! Sint quaerat debitis commodi voluptate officia architecto dolor quos veritatis! Dolore fugiat illo velit vel est facere qui." />
+                <CarouselItem img="https://i.ibb.co/9c6CV5P/Christies-Tailors-Services.jpg" title="The size that fits you most" description="We are highly focused on making the clothes fit and totally perfect for you. However you are, your clothe are guaranteed to be fit. " />
+                <CarouselItem img="https://i.ibb.co/84t1xJK/pexels-photo-404159.jpg" title="Sizings and stuff through internet" description="All the information we need to make a clothe, we can collect through internet. Send your clothe information to use through email or any online system and we get it. Also you can get us on video call to clarify things about your clothe." />
+                <CarouselItem img="https://i.ibb.co/Zc1Bx5R/D66-UE7iz-Hh-Sk-Uu-GM08ura3-Dhk-Y9-Mo-U8-UKQDRf-Ma-E1.jpg" title="24/7 work hour and availability." description="We work every hour we have. Whenever you need a service, just contact and you get it everything alright. We response within an hour." />
             </Carousel>
         </div>
     );
